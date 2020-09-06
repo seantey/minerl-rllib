babe73d9da24681b9567b0130877d99d621d6a17
Sean Tey <stey@dons.usfca.edu>
2020-09-06 15:01:04 -0700

    moved main files to folder to package with setup tools

--- models/torch/reward.py
+++
@@ -1,9 +0,0 @@
-import torch.nn as nn
-
-
-def baseline(reward_embed_size=128):
-    reward_network = nn.Sequential(
-        nn.Linear(1, reward_embed_size),
-        nn.Tanh(),
-    )
-    return reward_network, reward_embed_size
